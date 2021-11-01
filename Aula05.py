95d5c74ec126921b993733b312ba97c90f32a01d
Danilo Américo <93346408+DaniloAmerico@users.noreply.github.com>
2021-11-01 14:57:11 -0300

    Update Aula05.py

--- Aula05.py
+++ Aula05.py
@@ -1,2 +1,6 @@
-msg='Olá Mundo'
-print(msg)
+t = int(input('Digite até onde ira o Fibonacci: '))
+n=0
+while n < t:
+    FN = ( pow((1+pow(5,1/2))/2,n) - pow((1-pow(5,1/2))/2,n) ) / (pow(5,1/2))
+    n = n+1
+    print('{:.0f}'.format(FN))
